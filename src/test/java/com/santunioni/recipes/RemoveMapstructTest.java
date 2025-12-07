f44d8fd20f758be6fb9f7b307eb824c4c1cb5c44
Vinícius Vargas <vinicius.vargas@quintoandar.com>
2025-12-07 17:05:33 -0300

    Refactor `RemoveMapstructTest` to improve formatting and ensure consistent spacing in `SimpleDtoMapper` implementations.

--- src/test/java/com/santunioni/recipes/RemoveMapstructTest.java
+++ src/test/java/com/santunioni/recipes/RemoveMapstructTest.java
@@ -101,6 +101,7 @@ import javax.annotation.processing.Generated;
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17"
 )
 public class SimpleDtoMapperImpl implements SimpleDtoMapper {
+    
     @Override
     public SimpleDtoOut toSimpleDtoOut(SimpleDtoIn simpleDtoIn) {
         if (simpleDtoIn == null) {
@@ -112,6 +113,7 @@ public class SimpleDtoMapperImpl implements SimpleDtoMapper {
 
         return new SimpleDtoOut(id, name);
     }
+    
     @Override
     public SimpleDtoIn toSimpleDtoIn(SimpleDtoOut simpleDtoOut) {
         if (simpleDtoOut == null) {
@@ -149,41 +151,44 @@ public class SimpleDtoMapperImpl implements SimpleDtoMapper {
               """,
             // language=java
             """
- package com.santunioni.fixtures.dtoMappers;
- 
- import javax.annotation.processing.Generated;
- 
- import org.mapstruct.Mapper;
- 
- @Generated(
-     value = "org.mapstruct.ap.MappingProcessor",
-     date = "2025-01-01T00:00:00Z",
-     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17"
- )
- public class SimpleDtoMapper {
- 
-     public SimpleDtoOut toSimpleDtoOut(SimpleDtoIn simpleDtoIn) {
-         if (simpleDtoIn == null) {
-             return null;
-         }
- 
-         Long id = simpleDtoIn.getId();
-         String name = simpleDtoIn.getName();
- 
-         return new SimpleDtoOut(id, name);
-     }
- 
-     public SimpleDtoIn toSimpleDtoIn(SimpleDtoOut simpleDtoOut) {
-         if (simpleDtoOut == null) {
-             return null;
-         }
- 
-         Long id = simpleDtoOut.getId();
-         String name = simpleDtoOut.getName();
- 
-         return new SimpleDtoIn(id, name);
-     }
- }""",
+package com.santunioni.fixtures.dtoMappers;
+
+import javax.annotation.processing.Generated;
+
+import org.mapstruct.Mapper;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-01T00:00:00Z",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17"
+)
+public class SimpleDtoMapper {
+
+
+    public SimpleDtoOut toSimpleDtoOut(SimpleDtoIn simpleDtoIn) {
+        if (simpleDtoIn == null) {
+            return null;
+        }
+
+        Long id = simpleDtoIn.getId();
+        String name = simpleDtoIn.getName();
+
+        return new SimpleDtoOut(id, name);
+    }
+
+
+    public SimpleDtoIn toSimpleDtoIn(SimpleDtoOut simpleDtoOut) {
+        if (simpleDtoOut == null) {
+            return null;
+        }
+
+        Long id = simpleDtoOut.getId();
+        String name = simpleDtoOut.getName();
+
+        return new SimpleDtoIn(id, name);
+    }
+}
+""",
             spec -> spec.path("src/main/java/com/santunioni/fixtures/dtoMappers/SimpleDtoMapper.java")
           )
         );
