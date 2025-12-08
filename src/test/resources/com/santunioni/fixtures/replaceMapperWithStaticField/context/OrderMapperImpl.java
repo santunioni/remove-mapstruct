1f9bf931d0819e37d56324d3beb2cf86ed919666
Vinícius Vargas <vinicius.vargas@quintoandar.com>
2025-12-08 18:46:58 -0300

    Refactor mapper implementations: adjust indentation for consistency and add a missing newline in `SimpleDtoMapperImpl`.

--- src/test/resources/com/santunioni/fixtures/replaceMapperWithStaticField/context/OrderMapperImpl.java
+++ src/test/resources/com/santunioni/fixtures/replaceMapperWithStaticField/context/OrderMapperImpl.java
@@ -3,9 +3,9 @@ package com.santunioni.fixtures.dtoMappers;
 import javax.annotation.processing.Generated;
 
 @Generated(
-    value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-01T00:00:00Z",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17"
+        value = "org.mapstruct.ap.MappingProcessor",
+        date = "2025-01-01T00:00:00Z",
+        comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17"
 )
 public class OrderMapperImpl implements OrderMapper {
 
